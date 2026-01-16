ab5fd97a5b4a67072743b5c2efc500a4d2699c9d
clarinetgamer <54485479+clarinetgamer@users.noreply.github.com>
2026-01-16 15:42:31 -0700

    wide

--- phoneobooth/phoneobooth.js
+++ phoneobooth/phoneobooth.js
@@ -160,7 +160,7 @@ function mainWindow() {
   image(mainbg, 0, 0, ratioScale*1080, ratioScale*1840);
   push();
   scale(-1, 1);
-  image(capture, -ratioScale*83, ratioScale*942, -ratioScale*654, ratioScale*443, 0, 0, capture.width, capture.width*0.75);
+  image(capture, -ratioScale*83, ratioScale*942, -ratioScale*654, ratioScale*443);
   pop();
   textSize(250*ratioScale);
   text(timer, 227*ratioScale, 560*ratioScale);
@@ -220,14 +220,14 @@ function resetVars() {
 function printingScreen() {
   cnvBuild = createCanvas(1200, 1800);
   image(printTemp, 0, 0, 1200, 1800);
-  image(snap1, 50, 45, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap1, 650, 45, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap2, 50, 458, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap2, 650, 458, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap3, 50, 871, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap3, 650, 871, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap4, 50, 1284, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap4, 650, 1284, 501, 376,0, 0, capture.width, capture.width*.75);
+  image(snap1, 50, 45, 501, 376);
+  image(snap1, 650, 45, 501, 376);
+  image(snap2, 50, 458, 501, 376);
+  image(snap2, 650, 458, 501, 376);
+  image(snap3, 50, 871, 501, 376);
+  image(snap3, 650, 871, 501, 376);
+  image(snap4, 50, 1284, 501, 376);
+  image(snap4, 650, 1284, 501, 376);
   printSave = cnvBuild.get();
   printScreen = false;
   digiScreen = true;
@@ -236,10 +236,10 @@ function printingScreen() {
 function digifyScreen() {
   cnvBuildDig = createCanvas(600, 1800);
   image(digiTemp, 0, 0, 600, 1800);
-  image(snap1, 50, 45, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap2, 50, 458, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap3, 50, 871, 501, 376,0, 0, capture.width, capture.width*.75);
-  image(snap4, 50, 1284, 501, 376,0, 0, capture.width, capture.width*.75);
+  image(snap1, 50, 45, 501, 376);
+  image(snap2, 50, 458, 501, 376);
+  image(snap3, 50, 871, 501, 376);
+  image(snap4, 50, 1284, 501, 376);
   digiSave = cnvBuildDig.get();
   digiScreen = false;
   finalScreen = true;
