112d29a76674798e0416581730809ecff75ee408
clarinetgamer <54485479+clarinetgamer@users.noreply.github.com>
2026-03-01 18:17:48 -0500

    updating about for mrsnow site

--- about/about.js
+++ about/about.js
@@ -52,7 +52,7 @@ function mouseClicked() {
     } else if (buttonBounds(977, 217, 131, 26)) {
       window.open('https://drive.google.com/file/d/1WsKUzg5BFox6iBBhxvZxEZnhlS8QMoKl/view?usp=sharing', "_self");
     } else if (buttonBounds(977, 258, 131, 26)) {
-      window.open('https://www.instagram.com/p/DR7rhwjgYwj/?igsh=MWV6bDQwODJ4eTdnag==', "_self");
+      window.open('https://bradbedmusic.com/mrsnow', "_self");
     } else if (buttonBounds(977, 296, 131, 26)) {
       window.open("https://bradbedmusic.com/phoneobooth", "_self");
     }
